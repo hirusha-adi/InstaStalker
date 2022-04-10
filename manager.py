441e202d28ed655fe107cc2d45e04e5239c1be1d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-10 21:35:40 +0530

    improved save followers and followees funcs

--- manager.py
+++ manager.py
@@ -4,34 +4,7 @@ import sys
 from instaloader import Instaloader, Profile
 
 
-class Database:
-    config_filename = os.path.join(os.getcwd(), "config.json")
-
-    if not(os.path.isfile(config_filename)):
-        with open(config_filename, "w", encoding="utf-8") as _file:
-            _file.write(
-                '{\n\t"username": "",\n\t"password": ""\n\t"web": {\n\t\t"host": "0.0.0.0",\n\t\t"port": 8080,\n\t\t"debug": false\n\t}\n}')
-
-    with open(config_filename, "r", encoding="utf-8") as _file:
-        _data = json.load(_file)
-
-    USERNAME = _data["username"]
-    PASSWORD = _data["password"]
-
-    if USERNAME is None:
-        sys.exit(
-            "No username/email/phone-number is given to log-in. Please fill it in the `config.json` file")
-
-    if PASSWORD is None:
-        sys.exit(
-            "No passowrd is given to log-in. Please fill it in the `config.json` file")
-
-    HOST = _data["web"]["host"]
-    PORT = _data["web"]["port"]
-    DEBUG = _data["web"]["debug"]
-
-
-class Manager:
+class InstaProfile:
     def __init__(self, target: str = None):
         self.CLEAR = ("clear" if os.name == "posix" else "cls")
         self.TARGET = target
@@ -132,34 +105,221 @@ class Manager:
             self.followers = self.profile.get_followers()
         return self.followers
 
-    def saveFollowers(self):
+    def saveFollowers(self, formatting: str = None, mode: str = "simple", output: str = "print_only"):
+        """
+        `formatting`: str -->
+            count
+            username
+            full_name
+            userid
+            is_private
+            is_verified
+            meida_count
+            followers
+            followees
+            is_business_account
+            biography
+            profile_pic_url
+        
+        `mode`: str -->
+            low
+            mid
+            all/high
+
+        `output`: str -->
+            print_only
+            json
+            text
+        """
+
+        if formatting is None:
+            if mode == "low":
+                information = "{count} | {username} | {full_name}"
+            elif mode == "mid":
+                information = "{count} | {username} | {full_name} | {userid} | {is_private} | {is_verified} | {meida_count} | {followers} | {followees}"
+            elif mode == "all" or mode == "high":
+                information = "{count} | {username} | {full_name} | {userid} | {is_private} | {is_verified} | {meida_count} | {is_business_account} | {followers} | {followees} | {biography} | {profile_pic_url}"
+        else:
+            information = formatting
+
         followers = self.getFollowersList()
-        count = 1
-        with open(os.path.join(os.getcwd(), str(self.profile.username) + "_followers.txt"), "w", encoding="utf-8") as _file_followers:
+
+        if output == "print_only":
+            count = 1
             for profile in followers:
-                text = f"{count} | {profile.username} | {profile.full_name} | {profile.userid} | {'Private' if profile.is_private == True else 'Not Private'} | {'Verified' if profile.is_verified == True else 'Not Verified'} | {profile.mediacount} Media | {profile.followers} Followers | {profile.followees} Followees | {'Bussiness Account' if profile.is_business_account == True else 'Normal Account'} | " + \
-                    str('-'.join(profile.biography.split('\n'))) + \
-                    f" | {profile.profile_pic_url}"
+                text = information.format(
+                    count=count,
+                    username=profile.username,
+                    full_name=profile.full_name,
+                    userid=profile.userid,
+                    is_private=profile.is_private,
+                    is_verified=profile.is_verified,
+                    is_business_account=profile.is_business_account,
+                    meida_count=profile.mediacount,
+                    followers=profile.followers,
+                    followees=profile.followees,
+                    biography=profile.biography,
+                    profile_pic_url=profile.profile_pic_url
+                )                
                 print(text)
-                _file_followers.write(text + "\n")
                 count += 1
+        
+        if output == "json":
+            full_dict = {"data": []}
+            count = 1
+            for profile in followers:
+                text = information.format(
+                    count=count,
+                    username=profile.username,
+                    full_name=profile.full_name,
+                    userid=profile.userid,
+                    is_private=profile.is_private,
+                    is_verified=profile.is_verified,
+                    is_business_account=profile.is_business_account,
+                    meida_count=profile.mediacount,
+                    followers=profile.followers,
+                    followees=profile.followees,
+                    biography=profile.biography,
+                    profile_pic_url=profile.profile_pic_url
+                )                
+                print(text)
+                full_dict["data"].append(text)
+                count += 1
+            with open(os.path.join(os.getcwd(), str(self.profile.username) + "_followers.json"), "w", encoding="utf-8") as _file_followers:
+                json.dump(full_dict, _file_followers)
+
+        if output == "text":
+            with open(os.path.join(os.getcwd(), str(self.profile.username) + "_followers.txt"), "w", encoding="utf-8") as _file_followers:
+                count = 1
+                for profile in followers:
+                    text = information.format(
+                        count=count,
+                        username=profile.username,
+                        full_name=profile.full_name,
+                        userid=profile.userid,
+                        is_private=profile.is_private,
+                        is_verified=profile.is_verified,
+                        is_business_account=profile.is_business_account,
+                        meida_count=profile.mediacount,
+                        followers=profile.followers,
+                        followees=profile.followees,
+                        biography=profile.biography,
+                        profile_pic_url=profile.profile_pic_url
+                    )                
+                    print(text)
+                    _file_followers.write(text + "\n")
+                    count += 1
 
     def getFolloweesList(self):
         if self.followees:
             self.followees = self.profile.get_followees()
         return self.followees
 
-    def saveFollowees(self):
+    def saveFollowees(self, formatting: str = None, mode: str = "simple", output: str = "print_only"):
+        """
+        `formatting`: str -->
+            count
+            username
+            full_name
+            userid
+            is_private
+            is_verified
+            meida_count
+            followers
+            followees
+            is_business_account
+            biography
+            profile_pic_url
+        
+        `mode`: str -->
+            low
+            mid
+            all/high
+        
+        `output`: str -->
+            print_only
+            json
+            text
+        """
+
+        if formatting is None:
+            if mode == "low":
+                information = "{count} | {username} | {full_name}"
+            elif mode == "mid":
+                information = "{count} | {username} | {full_name} | {userid} | {is_private} | {is_verified} | {meida_count} | {followers} | {followees}"
+            elif mode == "all" or mode == "high":
+                information = "{count} | {username} | {full_name} | {userid} | {is_private} | {is_verified} | {meida_count} | {is_business_account} | {followers} | {followees} | {biography} | {profile_pic_url}"
+        else:
+            information = formatting
+
         followees = self.getFolloweesList()
-        count = 1
-        with open(os.path.join(os.getcwd(), str(self.profile.username) + "_followees.txt"), "w", encoding="utf-8") as _file_followees:
+
+        if output == "print_only":
+            count = 1
             for profile in followees:
-                text = f"{count} | {profile.username} | {profile.full_name} | {profile.userid} | {'Private' if profile.is_private == True else 'Not Private'} | {'Verified' if profile.is_verified == True else 'Not Verified'} | {profile.mediacount} Media | {profile.followers} Followers | {profile.followees} Followees | {'Bussiness Account' if profile.is_business_account == True else 'Normal Account'} | " + \
-                    str('-'.join(profile.biography.split('\n'))) + \
-                    f" |{profile.profile_pic_url}"
+                text = information.format(
+                    count=count,
+                    username=profile.username,
+                    full_name=profile.full_name,
+                    userid=profile.userid,
+                    is_private=profile.is_private,
+                    is_verified=profile.is_verified,
+                    is_business_account=profile.is_business_account,
+                    meida_count=profile.mediacount,
+                    followers=profile.followers,
+                    followees=profile.followees,
+                    biography=profile.biography,
+                    profile_pic_url=profile.profile_pic_url
+                )                
                 print(text)
-                _file_followees.write(text + "\n")
                 count += 1
+        
+        if output == "json":
+            full_dict = {"data": []}
+            count = 1
+            for profile in followees:
+                text = information.format(
+                    count=count,
+                    username=profile.username,
+                    full_name=profile.full_name,
+                    userid=profile.userid,
+                    is_private=profile.is_private,
+                    is_verified=profile.is_verified,
+                    is_business_account=profile.is_business_account,
+                    meida_count=profile.mediacount,
+                    followers=profile.followers,
+                    followees=profile.followees,
+                    biography=profile.biography,
+                    profile_pic_url=profile.profile_pic_url
+                )                
+                print(text)
+                full_dict["data"].append(text)
+                count += 1
+            with open(os.path.join(os.getcwd(), str(self.profile.username) + "_followees.json"), "w", encoding="utf-8") as _file_followees:
+                json.dump(full_dict, _file_followees)
+
+
+        if output == "text":
+            with open(os.path.join(os.getcwd(), str(self.profile.username) + "_followees.txt"), "w", encoding="utf-8") as _file_followees:
+                count = 1
+                for profile in followees:
+                    text = information.format(
+                        count=count,
+                        username=profile.username,
+                        full_name=profile.full_name,
+                        userid=profile.userid,
+                        is_private=profile.is_private,
+                        is_verified=profile.is_verified,
+                        is_business_account=profile.is_business_account,
+                        meida_count=profile.mediacount,
+                        followers=profile.followers,
+                        followees=profile.followees,
+                        biography=profile.biography,
+                        profile_pic_url=profile.profile_pic_url
+                    )  
+                    print(text)
+                    _file_followees.write(text + "\n")
+                    count += 1
 
     def saveProfileInfo(self):
         data = self.getProfileInfo()
@@ -171,3 +331,40 @@ class Manager:
         self.saveProfileInfo()
         self.saveFollowers()
         self.saveFollowees()
+
+
+class Database:
+    config_filename = os.path.join(os.getcwd(), "config.json")
+
+    if not(os.path.isfile(config_filename)):
+        with open(config_filename, "w", encoding="utf-8") as _file:
+            _file.write(
+                '{\n\t"username": "",\n\t"password": ""\n\t"web": {\n\t\t"host": "0.0.0.0",\n\t\t"port": 8080,\n\t\t"debug": false\n\t}\n}')
+
+    with open(config_filename, "r", encoding="utf-8") as _file:
+        _data = json.load(_file)
+
+    USERNAME = _data["username"].strip()
+    PASSWORD = _data["password"].strip()
+        
+    if USERNAME is None:
+        username_file = os.path.join(os.getcwd(), "username.txt")
+        if os.path.isfile(username_file):
+            with open(username_file, "r", encoding="utf-8") as _uname_file:
+                USERNAME = _uname_file.read().strip()
+        else:
+            sys.exit(
+                "No username/email/phone-number is given to log-in. Please fill it in the `config.json` file or have it in the './username.txt' file")
+
+    if PASSWORD is None:
+        password_file = os.path.join(os.getcwd(), "password.txt")
+        if os.path.isfile(password_file):
+            with open(password_file, "r", encoding="utf-8") as _passwd_file:
+                PASSWORD = _uname_file.read().strip()
+        else:
+            sys.exit(
+                "No passowrd is given to log-in. Please fill it in the `config.json` file or have it in the './password.txt' file.")
+
+    HOST = _data["web"]["host"]
+    PORT = _data["web"]["port"]
+    DEBUG = _data["web"]["debug"]
