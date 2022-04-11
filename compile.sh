81471b3eb00a3b74ec8eed8ed12ddf0ba82f0d2b
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-11 22:28:10 +0530

    remove unwanted files

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 python3 -m pip install PyInstaller
 python3 -m PyInstaller --onefile --console --noconfirm InstaStalker.py
+rm -rf ./build
+rm ./InstaStalker.spec -f
+rm ./__pycache__ -rf
 echo "COMPILED SUCCESSFULLY!"
