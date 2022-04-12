235bea3937f972616705ff950286e073824ae3d4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-12 09:15:43 +0530

    complete new UI

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template
 from manager import InstaProfile, Database
+import requests
+import os
+
 app = Flask(__name__)
 
 
-obj = InstaProfile()
-obj.setTARGET("ac3.desu")
-obj.login(username=Database.USERNAME, password=Database.PASSWORD)
-obj.processProfile()
-accinfo = obj.getProfileInfo()
+targte_username = "hirushaadi"
+
+# obj = InstaProfile()
+# obj.setTARGET("hirushaadi")
+# obj.login(username=Database.USERNAME, password=Database.PASSWORD)
+
+# Main Account Info
+print("[*] Please wait while target's profile information is being loaded")
+# accinfo = obj.getProfileInfo()
+print("[+] Loaded all basic target's profile information!")
 
 
 @app.route("/")
 def index():
+
     return render_template(
         "index.html",
-        accinfo=accinfo
+        # accinfo=accinfo,
+        # profile_image="file://" + os.path.join(os.getcwd(), "profilepic")
     )
 
 
