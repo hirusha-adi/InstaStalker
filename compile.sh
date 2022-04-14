25e4ca49b74ea67e0605b28523c53b7f8e4a0472
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-14 10:48:15 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/bash
 python3 -m pip install PyInstaller
-python3 -m PyInstaller --onefile --console --noconfirm InstaStalker.py
+python3 -m PyInstaller --noconfirm --onefile --console --name instastalk ./instastalk.py
+rm -f ./instastalk.spec
 rm -rf ./build
-rm ./InstaStalker.spec -f
-rm ./__pycache__ -rf
-echo "COMPILED SUCCESSFULLY!"
+folder_name="$(lsb_release -ds)"
+mkdir compiled && cd compiled
+mkdir "./$folder_name" && cd ..
+mv "dist/instastalk" "./compiled/$folder_name"
+rm -rf dist
+echo "[?] Do you want to Installed the compiled file[Y/n]?: "
+read installyn
+if [ $installyn = "y" ]; then
+    chmod +x "./compiled/$folder_name/instastalk"
+    sudo cp "./compiled/$folder_name/instastalk" "/usr/bin/instastalk"
+    echo "[+] Compiled and Installed!"
+else
+    echo "[+] Compiled!"
+fi
