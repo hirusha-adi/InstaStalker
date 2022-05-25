8e3db4e344f2b52554b8bc6117950a2dbc874f61
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-25 19:20:15 +0530

    set name

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 python3 -m pip install PyInstaller
-python3 -m PyInstaller --onefile --console --noconfirm InstaStalker.py
+python3 -m PyInstaller --onefile --console --noconfirm InstaStalker.py --name InstaStalker
 rm -rf ./build
 rm ./InstaStalker.spec -f
 rm ./__pycache__ -rf
